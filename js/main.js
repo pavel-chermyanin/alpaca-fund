02e04e6e3f4f375cce7ce0f8d8f029398d1223da
Pavel Che <alien.killer3@bk.ru>
2022-02-13 11:46:42 +0300

    changed gif on mp4

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ $(function () {
 
     // if the page is main start video
 
-    const runVideo = () => {
-        if ($('.top__video').length) {
-            $('.top__video').get(0).play()
-        }
-    }
-    runVideo()
+    // const runVideo = () => {
+    //     if ($('.top__video').length) {
+    //         $('.top__video').get(0).play()
+    //     }
+    // }
+    // runVideo()
 
     // connected animate library
 
@@ -19,9 +19,14 @@ $(function () {
 
     // animate for product-page__todo-item
 
-    $('.product-page__todo-item span').each(function(index, item) {
-        setTimeout(() => {
-            item.style.opacity = '1';
-        }, (index + 2) * 300);
-    })
+
+    if ($(window).width() > 768) {
+        console.log('!')
+        $('.product-page__todo-item span').each(function(index, item) {
+            setTimeout(() => {
+                item.style.opacity = '1';
+            }, (index + 2) * 300);
+        })
+    }
+
 })
