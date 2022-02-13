012eac9ab5fa63b5c54c2b723347d42fd20247a2
Pavel Che <alien.killer3@bk.ru>
2022-02-13 10:42:44 +0300

    finish the second page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 $(function () {
 
+
+
+    // if the page is main start video
+
     const runVideo = () => {
-         $('.top__video').get(0).play()
+        if ($('.top__video').length) {
+            $('.top__video').get(0).play()
+        }
     }
     runVideo()
-    console.log($('.top__video')[0])
+
+    // connected animate library
+
+    new WOW().init();
+
+
+
+    // animate for product-page__todo-item
+
+    $('.product-page__todo-item span').each(function(index, item) {
+        setTimeout(() => {
+            item.style.opacity = '1';
+        }, index * 300);
+    })
 })
